c23e992bf59d00348807f84390ab8425e51be4d7
Javier-Cs <krchipullajavier@gmail.com>
2025-06-12 16:10:06 -0500

    Implementación de userDetails en la entidad User para la gestión de roles y usuarios. Implementación de un enum con los roles necesarios

---
+++ src/main/java/com/example/ejemploSecurity/Entity/Role.java
@@ -0,0 +1,6 @@
+package com.example.ejemploSecurity.Entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/ejemploSecurity/Entity/User.java
@@ -0,0 +1,65 @@
+package com.example.ejemploSecurity.Entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name="User_tbl")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String lastNameUser;
+    private String emailUser;
+    private String passwordUser;
+
+    @Enumerated(EnumType.ORDINAL)
+    Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordUser;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
