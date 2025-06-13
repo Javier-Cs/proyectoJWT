649078904fc32017da7df3a3298bbfaf9941b9f3
Javier-Cs <krchipullajavier@gmail.com>
2025-06-13 11:16:53 -0500

    agregamos el metodo que genera los token para poder acceder a los comtrollers necesarios implementacion de configuraciones del app

---
+++ src/main/java/com/example/ejemploSecurity/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.example.ejemploSecurity.config;
+
+import com.example.ejemploSecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findUserByEmail(username)
+                .orElseThrow( () -> new UsernameNotFoundException("User not found"));
+    }
+}
--- src/main/java/com/example/ejemploSecurity/config/JwtService.java
+++ src/main/java/com/example/ejemploSecurity/config/JwtService.java
@@ -2,17 +2,36 @@ package com.example.ejemploSecurity.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "a66e8cb9491371963a265001ce2f09935811ec0b8592edd6cdc2c7e9d99ceb73";
+
+    public String generarToken(UserDetails userDetails) {
+        return generarToken(new HashMap<>(), userDetails);
+    }
+
+    public String generarToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder().setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*42))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
 
     public String getUserName(String token) {
         return getClaim(token, Claims::getSubject);
