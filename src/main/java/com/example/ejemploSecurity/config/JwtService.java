b367074c26f9ba91cb158f6768b41dc8ece6699d
Javier-Cs <krchipullajavier@gmail.com>
2025-06-12 22:10:25 -0500

    agregacion de dependencias - jjwt-api  -> proporciona las interfaces - jjwt-impl -> proporciona las implementaciones de las interfaces - jjwt-jackson -> se agrega para la serializacion y deserealizacion en JWT

--- src/main/java/com/example/ejemploSecurity/config/JwtService.java
+++ src/main/java/com/example/ejemploSecurity/config/JwtService.java
@@ -1,6 +1,13 @@
 package com.example.ejemploSecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -9,5 +16,23 @@ public class JwtService {
 
     public String getUserName(String token) {
         return getClaim(token, Claims::getSubject);
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T>  claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
